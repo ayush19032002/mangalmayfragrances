f6c339620062c05e70f75c89dbfab33aa150ffd4
Ayush Raghuvanshi <ayushrajdev2@gmail.com>
2026-04-23 10:55:24 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    allowedHosts: ["78f0-2409-40c1-1003-a053-7ddf-4b31-85ef-538.ngrok-free.app"]
-  }
 })
